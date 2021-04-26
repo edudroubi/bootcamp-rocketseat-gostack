dff54a463f079cb376097aa9c5771bc84d663d33
edudroubi <eduardodroubi@gmail.com>
2021-04-26 17:10:33 -0300

    Initial commit

---
+++ frontend/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use:{
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: [
+          { loader: 'file-loader' }
+        ]
+      }
+    ]
+  }
+};
+
+/*
+* When using webpack-cli 4 or webpack 5, change webpack-dev-server --mode development to webpack serve --mode development.
+*/
